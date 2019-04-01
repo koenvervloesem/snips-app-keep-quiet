eb5813383c40287535bb4d2b0c74d1600af801f0
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2019-04-02 00:39:47 +0200

    Create de.py

---
+++ translations/de.py
@@ -0,0 +1,12 @@
+"""
+This module contains the result sentences and intents for the German version
+of the Keep quiet app.
+"""
+
+# Result sentences
+RESULT_QUIET = "Ok, ich bin ruhig."
+RESULT_TALK = "Ok, ich spreche wieder."
+
+# Intents
+INTENT_QUIET = 'koan:Quiet'
+INTENT_TALK = 'koan:Talk'
